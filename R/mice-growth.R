7b6ce79789baef1966de8fe63bc7df66bcfacca1
roliveros-ramos <ricardo.oliveros@gmail.com>
2018-04-18 16:20:57 +0200

    growth with linear rate for small ones

--- R/mice-growth.R
+++ R/mice-growth.R
@@ -1,2 +1,10 @@
 
-VB = function(age, Linf, k, t0) Linf*(1-exp(-k*(age-t0)))
+VB = function(age, Linf, k, t0, thr=0.5, egg=1e-4) {
+
+  ref = Linf*(1-exp(-k*(thr-t0)))
+  out1 = ((ref-egg)/thr)*age + egg
+  out = Linf*(1-exp(-k*(age-t0)))
+  out[age<thr] = out1[age<thr]
+  return(out)
+
+}
