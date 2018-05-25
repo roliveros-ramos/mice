572c2e737d43791da8bc505684dd993e34dec20c
roliveros-ramos <ricardo.oliveros@gmail.com>
2018-05-25 18:06:07 +0200

    Rcpp implementation of the iterative algorithm.

---
+++ src/iterativeMortality.cpp
@@ -0,0 +1,71 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+// [[Rcpp::export]]
+SEXP iterativeMortality(SEXP rMj, SEXP radd, SEXP rw, SEXP rN, SEXP rYso, int rniter)
+{
+
+  NumericMatrix Mj(rMj);
+  NumericVector add(radd);
+  NumericVector w(rw);
+  NumericVector N(rN);
+  NumericVector Yso(rYso);
+  int niter = rniter;
+  int nrow = Mj.nrow();
+  int ncol = Mj.ncol();
+
+  NumericMatrix Cj(nrow, ncol);
+  NumericVector Yj(ncol);
+
+    for (int k=0; k<niter; k++)
+    {
+      NumericVector Zj(nrow);
+      NumericVector Dj(nrow);
+      for(int i=0; i<nrow; i++)
+      {
+        for(int j=0; j<ncol; j++)
+        {
+          Zj[i] += Mj(i,j);
+        }
+        Zj[i] += add[i];
+        Dj[i] = 1 - exp(-Zj[i]);
+      }
+
+      NumericVector cfj(ncol, 1.0);
+      double ans = 0.0;
+      for(int i=0; i<nrow; i++)
+      {
+        for(int j=0; j<ncol; j++)
+        {
+          ans = (Mj(i,j)/Zj[i])*Dj[i]*N[i];
+          if(traits::is_nan<REALSXP>(ans)) {
+            Cj(i,j) = 0;
+          } else {
+            Cj(i,j) = ans;
+          }
+          Yj[j] += w[i]*Cj(i,j);
+        }
+      }
+
+      double cfj_tmp = 0.0;
+      for(int j=0; j<ncol; j++)
+      {
+        cfj_tmp = Yso[j]/Yj[j];
+        if(traits::is_nan<REALSXP>(cfj_tmp)) cfj_tmp = 0;
+        if(cfj_tmp < 1) cfj[j] = cfj_tmp;
+      }
+
+      for(int i=0; i<nrow; i++)
+      {
+        for(int j=0; j<ncol; j++)
+        {
+          Mj(i,j) = cfj[j]*Mj(i,j);
+        }
+      }
+
+    }
+
+    return List::create(Named("M", Mj), Named("Yj", Yj));
+
+}
+
