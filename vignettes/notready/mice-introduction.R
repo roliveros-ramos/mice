2ad0560aa41238904e968fcc7888a01b67d907ed
roliveros-ramos <ricardo.oliveros@gmail.com>
2018-07-16 22:29:37 +0200

    delete vignettes

--- vignettes/notready/mice-introduction.R
+++
@@ -1,8 +0,0 @@
-## ----setup, include=FALSE------------------------------------------------
-knitr::opts_chunk$set(cache=TRUE)
-library(Rcpp)
-
-## ----evalCpp, eval=FALSE-------------------------------------------------
-#  library("Rcpp")
-#  evalCpp("2 + 2")
-
