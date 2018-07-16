1c45841f1399b177ea9ec1b1efa0dd9e5d87839b
roliveros-ramos <ricardo.oliveros@gmail.com>
2018-07-16 22:28:04 +0200

    update gitignore.

---
+++ vignettes/notready/mice-introduction.R
@@ -0,0 +1,8 @@
+## ----setup, include=FALSE------------------------------------------------
+knitr::opts_chunk$set(cache=TRUE)
+library(Rcpp)
+
+## ----evalCpp, eval=FALSE-------------------------------------------------
+#  library("Rcpp")
+#  evalCpp("2 + 2")
+
