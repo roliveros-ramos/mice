c660d0ae7bc1467035c921585d0f16c1f9e6a905
roliveros-ramos <ricardo.oliveros@gmail.com>
2018-08-17 23:57:35 +0200

    Ystar and delta are vectors now, can be specified by functional group. Bugs fixed.

--- src/getStartM.cpp
+++ src/getStartM.cpp
@@ -3,21 +3,29 @@ using namespace Rcpp;
 
 // [[Rcpp::export]]
 NumericMatrix getStartM(NumericVector N, NumericVector w,
-                        double delta, double dt, double Ystar=3.5) {
+                        NumericVector delta, double dt,
+                        NumericVector Ystar, NumericMatrix access) {
 
   int n = N.length();
   NumericMatrix M(n, n);
-  double Nj  = 0.0;
+  NumericVector Ys(n);
+  double Ni  = 0.0;
+  double dNi = 0.0;
   double Cij = 0.0;
+
+  for(int j=0; j < n; j++)
+  {
+    Ys[j] = Ystar[j]*w[j]*N[j]*dt;
+  }
 
   for(int i=0; i < n; i++)
   {
-    double Ysi = Ystar*w[i]*N[i]*dt;
+    Ni  = floor(N[i]);
+    dNi = delta[i]*Ni;
     for(int j=0; j < n; j++)
     {
-      Nj  = floor(N[j]);
-      Cij = floor(std::min(Ysi/w[j], delta*Nj));
-      if(Cij > 1) M(i,j) = log(Nj/(Nj-Cij));
+      Cij = floor(std::min(Ys[j]/w[i], dNi));
+      if(Cij > 1) M(i,j) = access(i,j)*log(Ni/(Ni-Cij));
     }
   }
 
