c89ecc403cdb7a1a712a9d0b69491645d4f68290
roliveros-ramos <ricardo.oliveros@gmail.com>
2018-08-30 19:56:31 +0200

    temporary plot...

--- R/mice-plot.R
+++ R/mice-plot.R
@@ -65,3 +65,37 @@ calculateTrophicEff = function(object) {
 
 }
 
+
+# Other plots -------------------------------------------------------------
+
+
+#' @export
+plotAll = function(x, skip=0, what="biomass", ...) {
+
+  sim = x
+  xtime = sim$time[-1] - 0.5*diff(sim$time)[1]
+  year = ceiling(sim$time[-1])
+  nr = sum(sim$groupTypes == "resource")
+  ns = ncol(sim$B) - nr
+  ind = xtime > skip
+
+  X = if(what=="biomass") sim$B else sim$Y
+
+  # par(mfrow=c(ns,1), mar=c(0,0,0,0), oma=c(3,4,1,4))
+
+  for(i in seq_len(ns)) {
+    icol =if(i!=7) i else 10
+    b = 1e-3*X[ind,i+nr]
+    ymax = max(b, na.rm=TRUE)
+    plot(xtime[ind], b, col=icol, axes=FALSE, type="l",
+         ylim=c(0,ymax))
+    axis(2*((i+1)%%2+1), las=1); box()
+    mtext(toupper(colnames(sim$B)[i+nr]), 3, adj=0.95, line=-1, cex=0.5, col=icol)
+  }
+  axis(1)
+
+  return(invisible())
+
+}
+
+
